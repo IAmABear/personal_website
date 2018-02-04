a1950b1b592518d85c3bb836dbba189cdcb19482
IAmABear <floyd.van.boksel@gmail.com>
2018-02-04 18:48:10 +0100

    Add styling to Profile

--- client/PageSections/CV/Profile/Profile.js
+++ client/PageSections/CV/Profile/Profile.js
@@ -3,7 +3,7 @@ import React from "react";
 import styles from "./Profile.scss";
 
 const Profile = () => (
-  <section className={styles["profile"]}>
+  <section className={`${styles["profile"]} flow-text`}>
     <h2>Profile</h2>
     <p>Creating (web)applications that don't u make go nuts when using them is my ultimate goal. Sadly i'm not an all-knowing guru... yet. In my quest to become a guru i'm currently specifically looking to expand my knowledge on responsive design, page optimalisation and animations. </p>
   </section>
