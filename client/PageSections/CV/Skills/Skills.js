d3338969cd86ba4023120dc1a837b0b68caa8692
IAmABear <floyd.van.boksel@gmail.com>
2018-02-04 20:26:01 +0100

    Add styling to Skills component

--- client/PageSections/CV/Skills/Skills.js
+++ client/PageSections/CV/Skills/Skills.js
@@ -2,23 +2,70 @@ import React from "react";
 
 import styles from "./Skills.scss";
 
+import Card from "./Card";
+
+const skills = [
+  {
+    title: "HTML(5)",
+    imagePath: "https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png",
+    score: 4,
+    experience: "+/- 7 years"
+  },
+  {
+    title: "CSS(3)",
+    imagePath: "http://iconshow.me/media/images/logo/brand-logo-icon/png/512/css3-512.png",
+    score: 3,
+    experience: "+/- 7 years"
+  },
+  {
+    title: "Balsamiq",
+    imagePath: "https://blog.balsamiq.com/wp-content/uploads/2017/09/Balsamiq-Wireframes-Thumbnail-Final-0.png",
+    score: 4,
+    experience: "+/- 6 years"
+  },
+  {
+    title: "Ruby",
+    imagePath: "https://ih0.redbubble.net/image.270426643.3373/flat,800x800,075,f.jpg",
+    score: 3,
+    experience: "+/- 4 years",
+    bigLogo: true
+  },
+  {
+    title: "React",
+    imagePath: "https://cdn.worldvectorlogo.com/logos/react.svg",
+    score: 3,
+    experience: "+/- 2 years"
+  },
+  {
+    title: "GraphQL",
+    imagePath: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/GraphQL_Logo.svg/1024px-GraphQL_Logo.svg.png",
+    score: 2,
+    experience: "+/- 1 years"
+  },
+  {
+    title: "Sass",
+    imagePath: "http://sass-lang.com/assets/img/styleguide/seal-color-aef0354c.png",
+    score: 4,
+    experience: "+/- 4 years",
+    bigLogo: true
+  },
+  {
+    title: "JavaScript",
+    imagePath: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Unofficial_JavaScript_logo.svg/480px-Unofficial_JavaScript_logo.svg.png",
+    score: 4,
+    experience: "+/- 7 years"
+  }
+];
+
 const Skills = () => (
   <section className={styles["skills"]}>
     <h2>Skills</h2>
 
-    <ul>
-      <li>HTML(5)</li>
-      <li>CSS(3)</li>
-      <li>JavaScript</li>
-      <li>Ruby (on Rails)</li>
-    </ul>
-
-    <ul>
-      <li>React</li>
-      <li>Sass</li>
-      <li>Haml</li>
-      <li>GraphQl</li>
-    </ul>
+    <section className={styles["current-skils"]}>
+      { skills.map((l) => (
+        <Card key={l.title} {...l} />
+      ))}
+    </section>
   </section>
 );
 
