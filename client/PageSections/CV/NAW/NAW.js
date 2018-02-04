f47b24d239de1e283dba58b7fed8d39cc68b8150
IAmABear <floyd.van.boksel@gmail.com>
2018-02-04 17:05:48 +0100

    Add basic styling to NAW

--- client/PageSections/CV/NAW/NAW.js
+++ client/PageSections/CV/NAW/NAW.js
@@ -5,14 +5,21 @@ import styles from "./NAW.scss";
 const NAW = () => (
   <section className={styles["naw"]}>
     <ul>
-      <li>
-        Mobile: +316 43058239
+      <li className={`${styles["name"]} flow-text`}>
+        <i className="small material-icons">account_box</i>
+        Floyd van Boksel
       </li>
-      <li>
-        Email: floyd.van.boksel@gmail.com
+      <li className={`${styles["phone"]} flow-text`}>
+        <i className="small material-icons">phone</i>
+        +316 43058239
       </li>
-      <li>
-        Residence: Amsterdam
+      <li className={`${styles["mail"]} flow-text`}>
+        <i className="small material-icons">mail</i>
+        floyd.van.boksel@gmail.com
+      </li>
+      <li  className={`${styles["residence"]} flow-text`}>
+        <i className="small material-icons">home</i>
+        Amsterdam, The Netherlands
       </li>
     </ul>
   </section>
