677d6daf4cca634770ee1c5e60a02fcd0c15cbd7
IAmABear <floyd.van.boksel@gmail.com>
2018-02-04 18:47:46 +0100

    Change Profile lorem ipsum with some usefull data

--- client/PageSections/CV/Profile/Profile.js
+++ client/PageSections/CV/Profile/Profile.js
@@ -5,7 +5,7 @@ import styles from "./Profile.scss";
 const Profile = () => (
   <section className={styles["profile"]}>
     <h2>Profile</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Creating (web)applications that don't u make go nuts when using them is my ultimate goal. Sadly i'm not an all-knowing guru... yet. In my quest to become a guru i'm currently specifically looking to expand my knowledge on responsive design, page optimalisation and animations. </p>
   </section>
 );
 
