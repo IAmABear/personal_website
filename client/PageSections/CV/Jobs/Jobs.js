a889a822058af61e307b0db6d1ccad558e6c0eb6
IAmABear <floyd.van.boksel@gmail.com>
2018-02-04 21:27:43 +0100

    Add Jobs styling

--- client/PageSections/CV/Jobs/Jobs.js
+++ client/PageSections/CV/Jobs/Jobs.js
@@ -1,20 +1,32 @@
 import React from "react";
 
+import Card from "../Components/Card";
+
 import styles from "./Jobs.scss";
+
+const jobs = [
+  {
+    title: "Werf3",
+    imagePath: "https://pbs.twimg.com/profile_images/967903155/Werf3_avatar_400x400.png",
+    subTitle: "UX designer",
+    text: "July 2013 - August 2013"
+  },
+  {
+    title: "GridLine",
+    imagePath: "https://www.gridline.nl/assets/products/GridLine-geenBeschrijving-178f6103d469ae5dd9d101921a88ba3c.svg",
+    subTitle: "UX designer / Front-end developer",
+    text: "March 2015 - present"
+  }
+];
 
 const Jobs = () => (
   <section className={styles["jobs"]}>
     <h2>Jobs</h2>
-    <article>
-      <h3>GridLine</h3>
-      <p>Front-end developer / UX designer</p>
-      <p>March 2015 - present</p>
-    </article>
-    <article>
-      <h3>Werf3</h3>
-      <p>UX designer</p>
-      <p>July 2013 - August 2013</p>
-    </article>
+    <section className={styles["jobs-overview"]}>
+      { jobs.map((j) => (
+        <Card key={j.title} {...j} />
+      ))}
+    </section>
   </section>
 );
 
