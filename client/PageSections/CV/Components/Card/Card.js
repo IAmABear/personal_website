17a806561aacca5d65d3253f8dd6be6d1759d0e4
IAmABear <floyd.van.boksel@gmail.com>
2018-03-03 14:07:42 +0100

    Add generic Card component

---
+++ client/PageSections/CV/Components/Card/Card.js
@@ -0,0 +1,28 @@
+import React, { PureComponent } from "react";
+
+import styles from "./Card.scss";
+
+class Card extends PureComponent {
+  render() {
+    const { imagePath, title, subTitle, text, horizontal } = this.props;
+
+    return (
+      <section className={styles["card-wrapper"]}>
+        <div className={`${styles["card"]} ${horizontal ? styles['-horizontal'] : ''} card `}>
+          <div className={`${styles["image"]} card-image`}>
+            <img src={ imagePath } />
+          </div>
+          <div className="card-stacked">
+            <div className="card-content">
+              <h4 className={styles["title"]}>{ title }</h4>
+              <h5 className="flow-text">{subTitle}</h5>
+              <p className="flow-text"><i>{ text }</i></p>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Card;
