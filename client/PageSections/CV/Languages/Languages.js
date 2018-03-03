a9aeb6c61157297fa6b4dfab41896e5ad722629a
IAmABear <floyd.van.boksel@gmail.com>
2018-03-03 14:19:31 +0100

    Add styling to languages page

--- client/PageSections/CV/Languages/Languages.js
+++ client/PageSections/CV/Languages/Languages.js
@@ -6,8 +6,8 @@ const Languages = () => (
   <section className={styles["languages"]}>
     <h2>Languages</h2>
     <ul>
-      <li>Dutch: Decent</li>
-      <li>English: Good</li>
+      <li className="flow-text">Dutch: Decent</li>
+      <li className="flow-text">English: Good</li>
     </ul>
   </section>
 );
