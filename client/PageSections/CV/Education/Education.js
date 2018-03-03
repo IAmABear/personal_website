0fd3db594029b2122451022d2daac42e8db2f2e1
IAmABear <floyd.van.boksel@gmail.com>
2018-03-03 13:58:56 +0100

    Add styling to Education

--- client/PageSections/CV/Education/Education.js
+++ client/PageSections/CV/Education/Education.js
@@ -1,26 +1,32 @@
 import React from "react";
 
+import Card from "../Components/Card";
+
 import styles from "./Education.scss";
 
-const Education = () => (
-  <section className={styles["education"]}>
-    <h2>Education</h2>
-    <article>
-      <h3>Hogeschool van Amsterdam Amsterdam</h3>
-      <ul>
-        <li>SEPTEMBER 2010 - JANUARI 2015</li>
-        <li>HBO Informatica</li>
-        <li>Field of study: Interaction Design</li>
-      </ul>
-    </article>
+const educations = [
+  {
+    title: "Hogeschool van Amsterdam",
+    imagePath: "https://upload.wikimedia.org/wikipedia/en/f/fc/Hogeschool_van_amsterdam_logo.svg",
+    subTitle: "Field of Study: Interaction Design",
+    text: "September 2010 - January 2015"
+  },
+  {
+    title: "Christelijke Scholengemeenschap Buitenveldert",
+    imagePath: "https://pbs.twimg.com/profile_images/378800000797681015/8e0b515fbaa0bb07bf0e6526455beea4_400x400.jpeg",
+    subTitle: "Havo",
+    text: "September 2010 - July 2014"
+  }
+];
 
-    <article>
-      <h3>Christelijke Scholengemeenschap Buitenveldert</h3>
-      <ul>
-        <li>SEPTEMBER 2014 - JULI 2010</li>
-        <li>Havo</li>
-      </ul>
-    </article>
+const Education = () => (
+  <section>
+    <h2>Education</h2>
+    <section className={styles["education"]}>
+      { educations.map((i) => (
+        <Card key={i.title} {...i} />
+      ))}
+    </section>
   </section>
 );
 
