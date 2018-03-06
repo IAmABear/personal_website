14a9f99975f6928d1c6b03ce3fc484fe8f3acba0
IAmABear <floyd.van.boksel@gmail.com>
2018-03-06 21:04:07 +0100

    Refactor Contact to display data based on a array

--- client/PageSections/Contact/Contact.js
+++ client/PageSections/Contact/Contact.js
@@ -3,26 +3,23 @@ import ScrollableAnchor from 'react-scrollable-anchor'
 
 import styles from "./Contact.scss";
 
+const contactText = [
+  { icon: "account_box", text: "Floyd van Boksel" },
+  { icon: "phone", text: "+316 43058239" },
+  { icon: "mail", text: "floyd.van.boksel@gmail.com" },
+  { icon: "home", text: "Amsterdam, The Netherlands" }
+];
+
 const Contact = () => (
   <ScrollableAnchor id="Contact">
     <section className={styles["contact"]}>
       <ul>
-        <li className="flow-text">
-          <i className="small material-icons">account_box</i>
-          Floyd van Boksel
-        </li>
-        <li className="flow-text">
-          <i className="small material-icons">phone</i>
-          +316 43058239
-        </li>
-        <li className="flow-text">
-          <i className="small material-icons">mail</i>
-          floyd.van.boksel@gmail.com
-        </li>
-        <li  className="flow-text">
-          <i className="small material-icons">home</i>
-          Amsterdam, The Netherlands
-        </li>
+        { contactText.map((contactText, i) => (
+          <li key={i} className="flow-text">
+            <i className="small material-icons">{ contactText.icon }</i>
+            { contactText.text }
+          </li>
+        ))}
       </ul>
     </section>
   </ScrollableAnchor>
