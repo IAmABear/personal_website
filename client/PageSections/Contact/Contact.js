c563ed6411eb87245e8139799651b001b5c7005c
IAmABear <floyd.van.boksel@gmail.com>
2018-03-06 20:58:30 +0100

    Remove redundant css classes in Contact

--- client/PageSections/Contact/Contact.js
+++ client/PageSections/Contact/Contact.js
@@ -7,19 +7,19 @@ const Contact = () => (
   <ScrollableAnchor id="Contact">
     <section className={styles["contact"]}>
       <ul>
-        <li className={`${styles["name"]} flow-text`}>
+        <li className="flow-text">
           <i className="small material-icons">account_box</i>
           Floyd van Boksel
         </li>
-        <li className={`${styles["phone"]} flow-text`}>
+        <li className="flow-text">
           <i className="small material-icons">phone</i>
           +316 43058239
         </li>
-        <li className={`${styles["mail"]} flow-text`}>
+        <li className="flow-text">
           <i className="small material-icons">mail</i>
           floyd.van.boksel@gmail.com
         </li>
-        <li  className={`${styles["residence"]} flow-text`}>
+        <li  className="flow-text">
           <i className="small material-icons">home</i>
           Amsterdam, The Netherlands
         </li>
