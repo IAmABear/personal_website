254d89b0702ed5510fd539ba3de7ac9461f904d8
IAmABear <floyd.van.boksel@gmail.com>
2018-03-06 21:32:32 +0100

    Refactor NAW component

--- client/CV/NAW/NAW.js
+++ client/CV/NAW/NAW.js
@@ -2,25 +2,22 @@ import React from "react";
 
 import styles from "./NAW.scss";
 
+const nawData = [
+  { icon: "account_box", text: "Floyd van Boksel" },
+  { icon: "phone", text: "+316 43058239" },
+  { icon: "mail", text: "floyd.van.boksel@gmail.com" },
+  { icon: "home", text: "Amsterdam, The Netherlands" }
+];
+
 const NAW = () => (
   <section className={styles["naw"]}>
     <ul>
-      <li className="flow-text">
-        <i className="small material-icons">account_box</i>
-        Floyd van Boksel
-      </li>
-      <li className="flow-text">
-        <i className="small material-icons">phone</i>
-        +316 43058239
-      </li>
-      <li className="flow-text">
-        <i className="small material-icons">mail</i>
-        floyd.van.boksel@gmail.com
-      </li>
-      <li  className="flow-text">
-        <i className="small material-icons">home</i>
-        Amsterdam, The Netherlands
-      </li>
+      { nawData.map((naw, i) => (
+        <li key={i} className="flow-text">
+          <i className="small material-icons">{ naw.icon }</i>
+          { naw.text }
+        </li>
+      ))}
     </ul>
   </section>
 );
