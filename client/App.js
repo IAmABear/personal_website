c2438846ebd6c05649a9e510fa5e0fce06c922d6
IAmABear <floyd.van.boksel@gmail.com>
2018-03-06 21:15:46 +0100

    Move all components out of the PageSections directory
    This is an redundant layer.

--- client/App.js
+++ client/App.js
@@ -2,11 +2,9 @@ import React from "react";
 
 import styles from "./App.scss";
 
-import {
- Fold,
- CV,
- Contact
-} from "./PageSections";
+import Fold from "./Fold";
+import CV from "./CV";
+import Contact from "./Contact";
 
 const App = () => (
   <section className={styles["application"]}>
