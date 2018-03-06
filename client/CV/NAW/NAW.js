d42297973dc00c615a852990f9294ca094cb387a
IAmABear <floyd.van.boksel@gmail.com>
2018-03-06 21:25:39 +0100

    Refactor NAW styling
    This also removes the need for extra style classes per NAW item.

--- client/CV/NAW/NAW.js
+++ client/CV/NAW/NAW.js
@@ -5,19 +5,19 @@ import styles from "./NAW.scss";
 const NAW = () => (
   <section className={styles["naw"]}>
     <ul>
-      <li className={`${styles["name"]} flow-text`}>
+      <li className="flow-text">
         <i className="small material-icons">account_box</i>
         Floyd van Boksel
       </li>
-      <li className={`${styles["phone"]} flow-text`}>
+      <li className="flow-text">
         <i className="small material-icons">phone</i>
         +316 43058239
       </li>
-      <li className={`${styles["mail"]} flow-text`}>
+      <li className="flow-text">
         <i className="small material-icons">mail</i>
         floyd.van.boksel@gmail.com
       </li>
-      <li  className={`${styles["residence"]} flow-text`}>
+      <li  className="flow-text">
         <i className="small material-icons">home</i>
         Amsterdam, The Netherlands
       </li>
