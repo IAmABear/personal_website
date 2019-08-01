21344115bf6cbe9eabe4dad06ac08fe6d31a0f46
Floyd van Boksel <floyd.van.boksel@gmail.com>
2019-08-01 19:29:19 +0200

    Update CV with newest jobs

--- client/CV/Jobs/Jobs.js
+++ client/CV/Jobs/Jobs.js
@@ -7,15 +7,31 @@ import styles from "./Jobs.scss";
 const jobs = [
   {
     title: "Werf3",
-    imagePath: "https://pbs.twimg.com/profile_images/967903155/Werf3_avatar_400x400.png",
+    imagePath:
+      "https://pbs.twimg.com/profile_images/967903155/Werf3_avatar_400x400.png",
     subTitle: "UX designer",
     text: "July 2013 - August 2013"
   },
   {
     title: "GridLine",
-    imagePath: "https://www.gridline.nl/assets/products/GridLine-geenBeschrijving-178f6103d469ae5dd9d101921a88ba3c.svg",
+    imagePath:
+      "https://www.gridline.nl/assets/products/GridLine-geenBeschrijving-178f6103d469ae5dd9d101921a88ba3c.svg",
     subTitle: "UX designer / Front-end developer",
     text: "March 2015 - present"
+  },
+  {
+    title: "Saxum",
+    imagePath:
+      "https://media.licdn.com/dms/image/C560BAQEElCBg6etFtQ/company-logo_400_400/0?e=1572480000&v=beta&t=qborkAxIkRRdHGbuhltwb_X6ipxmd9k46vK9SV8hjOE",
+    subTitle: "Front-end developer",
+    text: "June 2018 - present"
+  },
+  {
+    title: "Transavia",
+    imagePath:
+      "https://www.transavia.com/static/img/logos/transavia-logo-vertical.svg",
+    subTitle: "Front-end developer",
+    text: "June 2018 - present"
   }
 ];
 
@@ -23,7 +39,7 @@ const Jobs = () => (
   <section className={styles["jobs"]}>
     <h2>Jobs</h2>
     <section className={styles["jobs-overview"]}>
-      { jobs.map((j) => (
+      {jobs.map(j => (
         <Card key={j.title} {...j} />
       ))}
     </section>
