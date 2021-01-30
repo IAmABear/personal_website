147585458c94e8a3b241af831685da0a9d7a28db
IAmNotABear <floyd.van.boksel@gmail.com>
2021-01-31 00:57:11 +0100

    Add snowpack optimize plugin

--- snowpack.config.js
+++ snowpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     public: '/',
     src: '/dist',
   },
-  plugins: ['@snowpack/plugin-svelte'],
+  plugins: ['@snowpack/plugin-svelte', "@snowpack/plugin-optimize"],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
