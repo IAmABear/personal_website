3a21fa9ced8eea82bd5377793d45754e0eae9562
IAmNotABear <floyd.van.boksel@gmail.com>
2021-02-28 19:25:43 +0100

    Change App to use tailwind css for the styling

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,23 +1,13 @@
 <script>
 </script>
 
-<div class="app">
-  <h1>This website is under construction</h1>
+<div class="flex flex-col items-center justify-center min-h-screen">
+  <h1 class="text-xl sm:text-2xl md:text-3xl">
+    This website is under construction
+  </h1>
   <img src="./images/bear_logo_transparant.png" alt="logo" />
-  <h2>Please bear with it</h2>
+  <h2 class="text-lg sm:text-xl md:text-3xl">Please bear with it</h2>
 </div>
 
 <style>
-  .app {
-    display: flex;
-    flex-flow: column;
-    justify-content: center;
-    align-items: center;
-  }
-
-  @media (max-width: 700px) {
-    img {
-      width: 100%;
-    }
-  }
 </style>
