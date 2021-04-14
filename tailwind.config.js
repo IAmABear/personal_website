f1ed375aeddd6bef49baaff6f172192e63af9bfc
IAmNotABear <floyd.van.boksel@gmail.com>
2021-04-14 17:30:05 +0200

    Add profile section to CV

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
     theme: {
       fontFamily: {
         'sans': ['Raleway', 'Helvetica', 'Arial', 'sans-serif']
+      },
+      rotate: {
+        '0': '0',
+        '45': '45deg',
+        '90': '90deg',
+        '180': '180deg',
+        '270': '270deg',
       }
     },
     variants: {},
