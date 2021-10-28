234667248a8b620b71320b2bc2a407cae70c170b
IAmNotABear <floyd.van.boksel@gmail.com>
2021-10-28 13:31:42 +0200

    Add favicons and robots.txt

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,34 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta lang="en" />
+        <meta name="description" content="Floyd van Boksel" />
+        <meta name="robots" content="nofollow" />
+
+        <meta name="theme-color" content="#FFFFFF" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicons/favicon-16x16.png"
+        />
+        <link
+          rel="mask-icon"
+          href="/images/favicons/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
 
       <Component {...pageProps} />
